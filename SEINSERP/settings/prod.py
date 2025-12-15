e939f32294fd8be2efe142f3ea56c2360a1aff2b
Sergio.Erazo <sierazo@hotmail.com>
2025-12-15 00:17:00 -0600

    update docker-compse.prod and prod.py

--- SEINSERP/settings/prod.py
+++ SEINSERP/settings/prod.py
@@ -29,6 +29,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
