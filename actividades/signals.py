a27c20e92180e69788908506cfc4f0f260e0c831
Sergio.Erazo <sierazo@hotmail.com>
2026-01-03 18:36:13 -0600

    refactor: Comentar señales y funciones relacionadas en signals.py

--- actividades/signals.py
+++ actividades/signals.py
@@ -1,9 +1,9 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import Ordendetrabajo
+#from django.db.models.signals import post_save
+#from django.dispatch import receiver
+#from .models import Ordendetrabajo
 
-@receiver(post_save, sender=Ordendetrabajo)
-def procesar_archivos_ot(sender, instance, created, **kwargs):
+#@receiver(post_save, sender=Ordendetrabajo)
+#def procesar_archivos_ot(sender, instance, created, **kwargs):
     # Solo cuando se crea o cuando hay imágenes
-    instance._procesar_imagenes()
-    instance._procesar_firmas()
+    #instance._procesar_imagenes()
+    #instance._procesar_firmas()
